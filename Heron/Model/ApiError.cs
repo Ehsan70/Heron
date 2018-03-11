9333389523c6d6facf882b0bd660dc70eff4b49e
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-03-11 02:11:18 -0700

    Exception handling

---
+++ Heron/Model/ApiError.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heron.Model
+{
+    public class ApiError
+    {
+        public string Message { get; set; }
+        public string Detail { get; set; }
+
+        // Ignoring stack track if it's null so set the null value handling to ignore
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
+        [DefaultValue("")]
+        public string StackTrace { get; set; }
+    }
+}
