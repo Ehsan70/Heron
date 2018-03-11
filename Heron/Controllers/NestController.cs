566c2d8a1fea24eb91043d4e9727cdabbccf68be
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-03-11 02:11:07 -0700

    Nest and Root Controllers

---
+++ Heron/Controllers/NestController.cs
@@ -0,0 +1,29 @@
+﻿using Heron.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heron.Controllers
+{
+    [Route("/[controller]")]
+    public class NestController : Controller
+    {
+        private readonly Nest _nest;
+
+        public NestController(IOptions<Nest> nestAccessor)
+        {
+            _nest = nestAccessor.Value;
+
+        }
+
+        [HttpGet(Name = nameof(GetNest))]
+        public IActionResult GetNest()
+        {
+            _nest.Href = Url.Link(nameof(GetNest), null);
+            return Ok(_nest);
+        }
+    }
+}
---
+++ Heron/Controllers/RootController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heron.Controllers
+{
+    [Route("/")]
+    public class RootController : Controller
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                nests = new
+                {
+                    href = Url.Link(nameof(NestController.GetNest), null)
+                }
+            };
+
+            return Ok(response);
+        }
+    }
+}
