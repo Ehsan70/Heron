37a098efcd5b5da2f6dcb573b3dff651f28923c7
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-03-11 02:09:59 -0700

    Basic models

---
+++ Heron/Model/Nest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heron.Model
+{
+    public class Nest : Resource
+    {
+        /// <summary> Name of the file where eggs are stored. </summary>
+        public string Name { get; set; }
+        public string Description { get; set; }
+        // public NestRoles nRole { get; set; }
+    }
+}
---
+++ Heron/Model/Resource.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heron.Model
+{
+    public abstract class Resource
+    {
+        // Setting the order to -2 means this property is at the top of serialized properties.  
+        [JsonProperty(Order = -2)]
+        public string Href { get; set; }
+    }
+}
