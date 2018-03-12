f14277385ac36b3c2f8d7ad418e4060dcfa249ba
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-03-12 00:21:08 -0700

    Nest Collection Model

---
+++ Heron/Model/NestCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heron.Model
+{
+    public class NestCollection : Resource
+    {
+        public string[] NestFiles { get; set; }
+    }
+}
